5581c5ff44f9962193829a6ffe343628c8e30c28
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2018-07-01 17:04:56 -0300

    Referencia del sistema SDK del propietario TinyMCE
    A ver que tal trabaja esto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="/path/to/tinymce/tinymce.min.js"></script>
+  <script type="text/javascript">
+  tinymce.init({
+    selector: '#mytextarea'
+  });
+  </script>
+</head>
+
+<body>
+<h1>TinyMCE by yuma2020</h1>
+  <form method="post">
+    <textarea id="mytextarea">Hello, World!</textarea>
+  </form>
+</body>
+</html>
